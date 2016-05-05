09ed2a5bb2d1c2113996c54ba85898045b3e71ef
lorenzop <lorenzo@poixson.com>
2016-05-05 16:38:32 -0400

    added main.htm template and page_blog.php files

---
+++ src/pages/page_blog.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * GrowControl Website
+ * @copyright 2004-2016
+ * @author lorenzo at poixson.com
+ * @link http://growcontrol.com/
+ */
+namespace pxn\gcWebsite\pages;
+
+
+class page_blog implements \pxn\phpUtils\portal\Page {
+
+
+
+	public function getPageContents() {
+		return 'BLOG';
+	}
+
+
+
+}
