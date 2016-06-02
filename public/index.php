9d73a0b7fb81aeebd27c5615367c84a6de0313dc
lorenzop <lorenzo@poixson.com>
2016-06-02 17:48:04 -0400

    added check for pxnloader.php to public/index.php file and added commented debug line

--- public/index.php
+++ public/index.php
@@ -1,5 +1,11 @@
 <?php
 
+if (!\file_exists('../pxnloader.php')) {
+	echo '<h2>File not found: pxnloader.php, run <i>composer update</i></h2>';
+	exit(1);
+}
 require('../pxnloader.php');
+
+//debug(TRUE);
 
 \pxn\gcWebsite\gcWebsite::autoinit();
