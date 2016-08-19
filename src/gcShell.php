885c9a6fa72a8fae92666b5aa0e7640ba85b5cf8
lorenzop <lorenzo@poixson.com>
2016-08-19 00:00:03 -0400

    added new gcShell class

---
+++ src/gcShell.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * GrowControl Website
+ * @copyright 2004-2016
+ * @author lorenzo at poixson.com
+ * @link http://growcontrol.com/
+ */
+namespace pxn\gcWebsite;
+
+
+class gcShell extends \pxn\phpUtils\app\ShellApp {
+
+
+
+	public function __construct() {
+		parent::__construct();
+	}
+
+
+
+}
