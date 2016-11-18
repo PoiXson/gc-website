161a991c0975fa4e5dd6165923c3e5c3aa45168f
lorenzop <lorenzo@poixson.com>
2016-11-18 18:53:03 -0500

    cleaned up public/index.php file

--- public/index.php
+++ public/index.php
@@ -1,12 +1,14 @@
 <?php
+// Place this file in public/ on your web server
 
 if (!\file_exists(__DIR__.'/../pxnloader.php')) {
-	echo "\n<h2>File not found: pxnloader.php, run <i>composer update</i></h2>\n";
+	echo "\n<h2>File not found: pxnloader.php, run <i>composer install</i></h2>\n\n";
 	exit(1);
 }
 require(__DIR__.'/../pxnloader.php');
 
+// uncomment to force debug mode
 //debug(TRUE);
 
-\pxn\gcWebsite\gcWebsite::init();
-\pxn\gcWebsite\gcShell::init();
+\pxn\gcWebsite\gcWebsite::register();
+\pxn\gcWebsite\gcShell::register();
