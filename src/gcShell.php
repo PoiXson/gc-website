a08139980a519b55a09f47e69579ecc2dd2f4e8b
lorenzop <lorenzo@poixson.com>
2016-12-13 10:30:21 -0500

    added db shell command to gcShell class (this will probably change)

--- src/gcShell.php
+++ src/gcShell.php
@@ -7,8 +7,51 @@
  */
 namespace pxn\gcWebsite;
 
+use pxn\phpUtils\ShellTools;
+use pxn\phpUtils\ShellHelp;
+use pxn\phpUtils\pxdb\dbCommands;
 
-class gcShell extends \pxn\phpPortal\Commands {
+
+class gcShell extends \pxn\phpUtils\app\ShellApp {
+
+
+
+	protected function doRender() {
+		$result = parent::doRender();
+		if ($result !== TRUE) {
+			return $result;
+		}
+
+		$arg = \strtolower(ShellTools::getArg());
+		if (empty($arg)) {
+			self::DisplayHelp();
+			return NULL;
+		}
+
+		// db command
+		if ($arg == 'db') {
+			$result = dbCommands::run();
+			if ($result !== TRUE) {
+				return FALSE;
+			}
+			$this->setRendered();
+			return TRUE;
+		}
+
+		if (ShellTools::isHelp()) {
+			self::DisplayHelp();
+			return NULL;
+		}
+		return FALSE;
+	}
+
+
+
+	public static function DisplayHelp() {
+//TODO:
+		ExitNow(1);
+	}
+
 
 
 }
